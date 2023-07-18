17fd289b49441341fa316b2efccdaac451003448
VIVIAN NJAU <viviannjau@users.noreply.github.com>
2023-07-18 13:01:48 +0200

    Landing page

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import styled from 'styled-components';
 
 const Landing = () => {
   return (
     <div>Landing</div>
   )
 }
+
+const landing = styled.div`
+  display: flex;
+
+`
 
 export default Landing
